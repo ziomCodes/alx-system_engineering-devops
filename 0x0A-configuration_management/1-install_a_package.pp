9767020f669a25ec8d76d5a5e5788f945a8c25b0
ziomCodes <ziomcode@gmail.com>
2023-06-28 11:19:52 +0000

    updated config

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# Installs puppet-lint
-
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+# install puppet-lint
+package{'puppet-lint':
+ensure   => '2.5.0',
+provider => 'gem'
 }

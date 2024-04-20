91d76e82badc7cc4c06fa90f79c8dca6c7d256ca
ziomCodes <ziomcode@gmail.com>
2024-04-20 14:24:04 +0000

    advance updates

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,18 +1,41 @@
 #!/usr/bin/python3
-"""
-queries the Reddit API and prints the titles of the first
-10 hot posts listed for a given subreddit
-"""
-from requests import get
+'''A module containing functions for working with the Reddit API.
+'''
+import requests
+
+
+BASE_URL = 'https://www.reddit.com'
+'''Reddit's base API URL.
+'''
 
 
 def top_ten(subreddit):
-    """prints titles of first 10 hot posts listed for a given subreddit"""
-    r_subreddit = get('http://www.reddit.com/r/{}/hot.json'.
-                      format(subreddit), headers={'User-Agent': 'Mybot'})
-    try:
-        subreddit_dict = r_subreddit.json()
-        for i in range(10):
-            print(subreddit_dict['data']['children'][i]['data']['title'])
-    except:
+    '''Retrieves the title of the top ten posts from a given subreddit.
+    '''
+    api_headers = {
+        'Accept': 'application/json',
+        'User-Agent': ' '.join([
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+            'AppleWebKit/537.36 (KHTML, like Gecko)',
+            'Chrome/97.0.4692.71',
+            'Safari/537.36',
+            'Edg/97.0.1072.62'
+        ])
+    }
+    sort = 'top'
+    limit = 10
+    res = requests.get(
+        '{}/r/{}/.json?sort={}&limit={}'.format(
+            BASE_URL,
+            subreddit,
+            sort,
+            limit
+        ),
+        headers=api_headers,
+        allow_redirects=False
+    )
+    if res.status_code == 200:
+        for post in res.json()['data']['children'][0:10]:
+            print(post['data']['title'])
+    else:
         print(None)

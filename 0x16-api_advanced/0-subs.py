2e1e98dcf051b4b760633bc01d90d74de95ccf35
ziomCodes <ziomcode@gmail.com>
2024-04-20 14:00:17 +0000

    advance updates

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
-"""
-Function that queries the Reddit API and returns the number of subscribers
-(not active users, total subscribers) for a given subreddit.
-If an invalid subreddit is given, the function should return 0
-"""
+# Defines number_of_subscribers function
 
-import requests
+import requests as r
 
 
 def number_of_subscribers(subreddit):
     """
-    Function that queries the Reddit API
-    - If not a valid subreddit, return 0.
-    """
-    req = requests.get(
-        "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-    )
+    queries the Reddit API for a subreddit's number of active subscribers.
 
-    if req.status_code == 200:
-        return req.json().get("data").get("subscribers")
-    else:
+    return(int): number of subreddit's subscribers, 0 if subreddit invalid
+    """
+    try:
+        g = r.get('https://api.reddit.com/r/{}/about'.format(subreddit),
+                  headers={'user-agent': 'python:v3.5.2 (by /u/maxastuart)'}
+                  )
+        g.raise_for_status()
+        return g.json().get('data').get('subscribers')
+    except Exception as e:
         return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
-"""
-Function that queries the Reddit API and prints the titles
-of the first 10 hot posts listed for a given subreddit.
-"""
+# Defines top_ten function
 
-import requests
+import requests as r
 
 
 def top_ten(subreddit):
     """
-    Function that queries the Reddit API
-    - If not a valid subreddit, print None.
+    queries the Reddit API and prints the titles of the first 10 hot posts
+    listed for a given subreddit
     """
-    req = requests.get(
-        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-        params={"limit": 10},
-    )
-
-    if req.status_code == 200:
-        for get_data in req.json().get("data").get("children"):
-            dat = get_data.get("data")
-            title = dat.get("title")
-            print(title)
-    else:
-        print(None)
+    try:
+        g = r.get('https://api.reddit.com/r/{}/hot?count=10'.format(subreddit),
+                  headers={'user-agent': 'python:v3.5.2 (by /u/maxastuart)'}
+                  )
+        g.raise_for_status()
+        for i in range(10):
+            print(g.json().get('data').get('children')[i].get('data')
+                  .get('title'))
+    except Exception as e:
+        print('None')
